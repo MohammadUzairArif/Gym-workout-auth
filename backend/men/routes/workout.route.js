7d93d04564774ddc7c1e00847a380c52a2a0cc12
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-05-05 02:46:25 +0500

    model added

--- backend/men/routes/workout.route.js
+++ backend/men/routes/workout.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-
+import WorkoutModel from "../models/workout.model.js";
 router.get("/", (req, res) => {
     res.status(200).json({ message: "Get all workouts" });
 })
@@ -10,8 +10,17 @@ router.get("/:id", (req, res) => {
     res.status(200).json({ message: "Get a single workout" });
 })
 // Create a new workout
-router.post("/", (req, res) => {
-    res.status(200).json({ message: "Create a new workout" });
+router.post("/",async (req, res) => {
+    const { title, load, reps } = req.body;
+    try {
+        const workout = new WorkoutModel({ title, load, reps });
+        await workout.save();
+        res.status(200).json(workout);
+    } catch (error) {
+        console.error(error)
+        res.status(400).json({ error: error.message });
+    }
+
 })
 // Update a workout
 router.patch("/:id", (req, res) => {
