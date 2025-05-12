2f21558a7121950e4bea21c15cfae38eab1cad1b
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-05-12 18:34:12 +0500

    logout added

--- frontend/src/components/ui/Navbar.jsx
+++ frontend/src/components/ui/Navbar.jsx
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../../hooks/uselogout';
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const handleLogout = () => {
+    logout();
+    
+  };
+
   return (
     <header className="bg-gray-900/90 backdrop-blur-md shadow-md sticky top-0 z-50 border-b border-gray-800">
       <div className="max-w-[1400px] mx-auto px-6 py-4 flex items-center justify-between font-poppins">
@@ -26,6 +33,12 @@ const Navbar = () => {
           >
             Sign Up
           </Link>
+          <button
+            onClick={handleLogout}
+            className="bg-red-500 hover:bg-red-600 text-white text-sm font-medium px-4 py-2 rounded-md transition-colors duration-200"
+          >
+            Logout
+          </button>
         </div>
       </div>
     </header>
